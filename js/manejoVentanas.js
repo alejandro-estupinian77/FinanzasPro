75363c47b44fc3804b0c4ad16665b44e76dd2b1b
aleja <aleja@DESKTOP-285EQC1>
2024-05-22 08:16:40 -0600

    Prueba 2. HTML App y JS

---
+++ js/manejoVentanas.js
@@ -0,0 +1,12 @@
+//Manejo de ventanas
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    const botonRegistroPagos = document.getElementById('boton-registroPagos');
+    const botonIngresosGastos = document.getElementById('boton-ingresosGastos');
+    const botonFunc3 = document.getElementById('boton-funcionalidad3');
+    const botonFunc4 = document.getElementById('boton-funcionalidad4');
+
+    console.log("Hola");
+
+});
